f0fdd038d71f1d5c320e379b650d65e955fbbeac
Francisco Arguedas Garita <francisco@MacBook-Pro-de-Francisco.local>
2022-02-12 23:22:06 -0600

    Registration activity advance

--- app/src/main/java/com/una/bookingapp/activity/RegistrationActivity.java
+++ app/src/main/java/com/una/bookingapp/activity/RegistrationActivity.java
@@ -2,11 +2,17 @@ package com.una.bookingapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.una.bookingapp.R;
+import com.una.bookingapp.controller.UserController;
+import com.una.bookingapp.model.User;
+
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -18,10 +24,40 @@ public class RegistrationActivity extends AppCompatActivity {
         init();
     }
 
-    public void init(){
+    public void init() {
         username = findViewById(R.id.editTextRegisterUsername);
         password = findViewById(R.id.editTextRegisterTextPassword);
         registerButton = findViewById(R.id.registerButtonConfirmation);
+
+        registerButtonConfirmation();
+
+    }
+
+    public void registerButtonConfirmation() {
+        registerButton.setOnClickListener((View v) -> {
+
+            runOnUiThread(() -> {
+                if (username.getText().toString().equals("") && password.getText().toString().equals("")) {
+                    username.setError("Vacio");
+                    password.setError("Vacio");
+                } else {
+                    String usernameStr = username.getText().toString();
+                    String passwordStr = password.getText().toString();
+
+                    User user = new User(usernameStr, passwordStr);
+
+                    /*new Thread( () -> {
+                        UserController.getInstance(getApplication()).addUser(user);
+                    }).start();
+*/
+
+                }
+
+
+            });
+
+
+        });
 
     }
 
