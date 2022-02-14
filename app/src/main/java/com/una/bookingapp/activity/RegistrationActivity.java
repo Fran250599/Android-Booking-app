2cc3b6c93b8e1dc15f94109380cc638fb4b8fbd8
Francisco_Arguedas <farguedas99@gmail.com>
2022-02-14 17:23:32 -0600

    Some verifications before updating the database - Registration activity

--- app/src/main/java/com/una/bookingapp/activity/RegistrationActivity.java
+++ app/src/main/java/com/una/bookingapp/activity/RegistrationActivity.java
@@ -15,6 +15,8 @@ import com.una.bookingapp.R;
 import com.una.bookingapp.controller.UserController;
 import com.una.bookingapp.model.User;
 
+import java.util.List;
+
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -39,10 +41,17 @@ public class RegistrationActivity extends AppCompatActivity {
         registerButton.setOnClickListener((View v) -> {
 
             runOnUiThread(() -> {
-                if (username.getText().toString().equals("") && password.getText().toString().equals("")) {
-                    username.setError("Vacio");
-                    password.setError("Vacio");
-                } else {
+
+                if(username.getText().toString().equals("") || password.getText().toString().equals("")){
+                    if (username.getText().toString().equals("")) {
+                        username.setError("Vacio");
+
+                    }else if(password.getText().toString().equals("")){
+                        password.setError("Vacio");
+                    }
+                }
+
+                else{
                     String usernameStr = username.getText().toString();
                     String passwordStr = password.getText().toString();
 
@@ -53,22 +62,41 @@ public class RegistrationActivity extends AppCompatActivity {
 
                     new Thread( () -> {
 
-                            try {
-                                long n = System.currentTimeMillis();
-                                synchronized (this) {
+                        try {
+                            long n = System.currentTimeMillis();
+                            synchronized (this) {
+
+                                List<User> users = UserController.getInstance(getApplication()).getUsers();
+                                boolean validUser= true;
+
+                                for(User auxUser : users){
+
+                                    if(auxUser.getUsername().equals(user.getUsername())){
+
+                                        runOnUiThread( () -> {
+                                            username.setError("Nombre de usuario ya existente. Por favor intente con otro.");
+                                        });
+                                        validUser = false;
+                                    }
+                                }
+
+                                if(validUser == true){
+                                    //UserController.getInstance(getApplication()).addUser(user);
 
                                     Snackbar snackbar = Snackbar.make(v, "Nuevo usuario agregado", Snackbar.LENGTH_SHORT);
                                     snackbar.show();
+
                                     System.out.println("waiting...");
                                     this.wait(3000); // change this to wait less, more, or indefinitely
 
                                     Intent intent = new Intent(this, MainActivity.class);
                                     startActivity(intent);
                                 }
-                                System.out.println("waited for " + (System.currentTimeMillis() - n) + "ms");
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
                             }
+                            System.out.println("waited for " + (System.currentTimeMillis() - n) + "ms");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }).start();
                 }
             });
